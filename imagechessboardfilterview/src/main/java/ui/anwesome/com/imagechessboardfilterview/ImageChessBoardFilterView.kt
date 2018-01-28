f1e52fcc63746c805727543fac13c5c2ece32be2
Anwesh43 <anweshthecool0@gmail.com>
2018-01-29 01:27:13 +0530

    adding ChessBoardSquareState

--- imagechessboardfilterview/src/main/java/ui/anwesome/com/imagechessboardfilterview/ImageChessBoardFilterView.kt
+++ imagechessboardfilterview/src/main/java/ui/anwesome/com/imagechessboardfilterview/ImageChessBoardFilterView.kt
@@ -60,4 +60,21 @@ class ImageChessBoardFilterView(ctx:Context,var bitmap:Bitmap):View(ctx) {
 
         }
     }
+    data class ChessBoardSquareState(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            if(dir == 0f) {
+                dir = 1-2*scale
+                startcb()
+            }
+        }
+    }
 }
