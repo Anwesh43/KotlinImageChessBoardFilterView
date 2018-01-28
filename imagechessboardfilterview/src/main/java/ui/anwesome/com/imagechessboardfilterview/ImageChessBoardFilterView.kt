07d9e1197aef37de102287dc033ebb7803b4a49e
Anwesh43 <anweshthecool0@gmail.com>
2018-01-29 01:20:18 +0530

    adding ChessFilterBoardSquare

--- imagechessboardfilterview/src/main/java/ui/anwesome/com/imagechessboardfilterview/ImageChessBoardFilterView.kt
+++ imagechessboardfilterview/src/main/java/ui/anwesome/com/imagechessboardfilterview/ImageChessBoardFilterView.kt
@@ -6,7 +6,7 @@ package ui.anwesome.com.imagechessboardfilterview
 import android.graphics.*
 import android.view.*
 import android.content.*
-class ImageChessBoardFilterView(ctx:Context):View(ctx) {
+class ImageChessBoardFilterView(ctx:Context,var bitmap:Bitmap):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
 
@@ -18,5 +18,22 @@ class ImageChessBoardFilterView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class ChessBoardSquare(var i:Int) {
+        fun draw(canvas:Canvas,paint:Paint,w:Float,scale:Float) {
+            var color = Color.parseColor("#99FFFFFF")
+            if(i%2 == 0) {
+                color = Color.parseColor("#99000000")
+            }
+            paint.color = color
+            val size = w/8
+            val x = (i%w)*size
+            val y = (i/w.toInt())*size
+            canvas.save()
+            val updating_size = (size/2)*scale
+            canvas.translate(x,y)
+            canvas.drawRect(RectF(size/2-updating_size,size/2-updating_size,size/2+updating_size,size/2+updating_size),paint)
+            canvas.restore()
+        }
     }
 }
