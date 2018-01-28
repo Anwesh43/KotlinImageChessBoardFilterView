668fa270806bffd767d8a6dc621730707a417774
Anwesh43 <anweshthecool0@gmail.com>
2018-01-29 01:25:10 +0530

    adding ChessBoardSquareContainer

--- imagechessboardfilterview/src/main/java/ui/anwesome/com/imagechessboardfilterview/ImageChessBoardFilterView.kt
+++ imagechessboardfilterview/src/main/java/ui/anwesome/com/imagechessboardfilterview/ImageChessBoardFilterView.kt
@@ -6,6 +6,8 @@ package ui.anwesome.com.imagechessboardfilterview
 import android.graphics.*
 import android.view.*
 import android.content.*
+import java.util.concurrent.ConcurrentLinkedQueue
+
 class ImageChessBoardFilterView(ctx:Context,var bitmap:Bitmap):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
@@ -34,6 +36,28 @@ class ImageChessBoardFilterView(ctx:Context,var bitmap:Bitmap):View(ctx) {
             canvas.translate(x,y)
             canvas.drawRect(RectF(size/2-updating_size,size/2-updating_size,size/2+updating_size,size/2+updating_size),paint)
             canvas.restore()
+        }
+    }
+    data class ChessBoardSquareContainer(var bitmap:Bitmap,var w:Float) {
+        val squares:ConcurrentLinkedQueue<ChessBoardSquare> = ConcurrentLinkedQueue()
+        init {
+            for(i in 0..63) {
+                squares.add(ChessBoardSquare(i))
+            }
+            bitmap = Bitmap.createScaledBitmap(bitmap,w.toInt(),w.toInt())
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.color = Color.BLACK
+            canvas.drawBitmap(bitmap,0f,0f,paint)
+            squares.forEach {
+                it.draw(canvas,paint)
+            }
+        }
+        fun update(stopcb:(Float)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
         }
     }
 }
