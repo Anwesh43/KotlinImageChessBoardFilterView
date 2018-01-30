1497c62201f621abe4ea264b647ca86c81c5f022
Anwesh43 <anweshthecool0@gmail.com>
2018-01-30 23:58:23 +0530

    adding AlternateChessBoardView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinimagechessboardfilterview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinimagechessboardfilterview/MainActivity.kt
@@ -4,17 +4,20 @@ import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import ui.anwesome.com.alternatechessboardview.AlternateChessBoardView
 import ui.anwesome.com.imagechessboardfilterview.ImageChessBoardFilterView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val view = ImageChessBoardFilterView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature_more))
-        view.addOnChessBoardListener({
-           Toast.makeText(this,"chess board square expanded",Toast.LENGTH_SHORT).show()
-        },{
-            Toast.makeText(this,"chess board square collapsed",Toast.LENGTH_SHORT).show()
-        })
+//        val view = ImageChessBoardFilterView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature_more))
+//        view.addOnChessBoardListener({
+//           Toast.makeText(this,"chess board square expanded",Toast.LENGTH_SHORT).show()
+//        },{
+//            Toast.makeText(this,"chess board square collapsed",Toast.LENGTH_SHORT).show()
+//        })
+        val view = AlternateChessBoardView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature_more))
+
     }
 }
