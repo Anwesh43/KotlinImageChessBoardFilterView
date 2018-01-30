48f53b7623efc43fad79fc199fb6d53b8a9128da
Anwesh43 <anweshthecool0@gmail.com>
2018-01-30 22:29:52 +0530

    using chessboardfilterlistener in app

--- app/src/main/java/ui/anwesome/com/kotlinimagechessboardfilterview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinimagechessboardfilterview/MainActivity.kt
@@ -3,12 +3,18 @@ package ui.anwesome.com.kotlinimagechessboardfilterview
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.imagechessboardfilterview.ImageChessBoardFilterView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ImageChessBoardFilterView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature_more))
+        val view = ImageChessBoardFilterView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature_more))
+        view.addOnChessBoardListener({
+           Toast.makeText(this,"chess board square expanded",Toast.LENGTH_SHORT).show()
+        },{
+            Toast.makeText(this,"chess board square collapsed",Toast.LENGTH_SHORT).show()
+        })
     }
 }
